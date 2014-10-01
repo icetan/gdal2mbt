7aa324f74f05768d30b4bcf8073ad1eb08267119
Christopher Fredén <c.freden@gmail.com>
2014-10-01 19:16:29 +0200

    Corrected some details in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup
 
 setup(
     name='gdal2mbt',
     version='0.1.0',
-    requires={
-        'Pillow': '>=2.5',
-        'GDAL': '>=1.8'
-    },
+    install_requires = ['Pillow>=2.5', 'GDAL>=1.8'],
     py_modules=['gdal2mbt'],
     scripts=['gdal2mbt'],
 
     description='Create MBTiles from GDAL files.',
     url='http://github.com/icetan/gdal2mbt/',
     license='MIT',
-    author='Chirstopher Freden',
+    author=u'Chirstopher Fredén',
     author_email='c.freden@gmail.com',
+    keywords = ['gdal', 'mbtiles', 'gis', 'tms'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
@@ -25,6 +26,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: GIS',
+        'Topic :: Scientific/Engineering :: GIS',
     ]
 )
